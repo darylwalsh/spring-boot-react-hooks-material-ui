db865f7c400684fcf3cee61c7f3b26b4f2bc04f8
Daryl <daryl@blpc.us>
2019-11-24 00:25:25 -0500

    feat(spring_boot_api): complete spring boot REST api
    completed backend REST api

--- cardatabse/src/main/java/com/llminol/cardatabse/CardatabseApplication.java
+++ cardatabse/src/main/java/com/llminol/cardatabse/CardatabseApplication.java
@@ -10,6 +10,8 @@ import com.llminol.cardatabse.domain.Car;
 import com.llminol.cardatabse.domain.CarRepository;
 import com.llminol.cardatabse.domain.Owner;
 import com.llminol.cardatabse.domain.OwnerRepository;
+import com.llminol.cardatabse.domain.User;
+import com.llminol.cardatabse.domain.UserRepository;
 
 @SpringBootApplication
 public class CardatabseApplication {
@@ -18,6 +20,10 @@ public class CardatabseApplication {
 
 	  @Autowired 
       private OwnerRepository orepository;
+	  
+	  @Autowired	
+	  private UserRepository urepository;	
+		
 
       public static void main(String[] args) {
         SpringApplication.run(CardatabseApplication.class, args);
@@ -35,6 +41,9 @@ public class CardatabseApplication {
 			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
 			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
 			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+			urepository.save(new User("user", "$2y$12$97ej1wotuF88C7.txO.jEugdTzjl1jNyqaaD4b9jNW1FKy2IdiD92", "USER"));
+			urepository.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
+
         };
       } 
     }
--- cardatabse/src/test/java/com/llminol/cardatabse/CardatabseApplicationTests.java
+++ cardatabse/src/test/java/com/llminol/cardatabse/CardatabseApplicationTests.java
@@ -1,16 +1,25 @@
 package com.llminol.cardatabse;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.llminol.cardatabse.web.CarController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CardatabseApplicationTests {
+	@Autowired
+	private CarController controller;
 
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
